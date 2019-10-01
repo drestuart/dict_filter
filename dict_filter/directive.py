a574cf406901d2853b3b0085a869829c124b3688
Dan Stuart <dstuart@barracuda.com>
2019-10-01 16:29:00 -0400

    Renamed ExtractDirective to GetChildDirective, added GetFromBaseDirective

--- dict_filter/directive.py
+++ dict_filter/directive.py
@@ -5,7 +5,6 @@ class Directive(ABC):
     Abstract base class for directives. Subclasses must override do_directive(element, base).
     """
 
-
     @abstractmethod
     def do_directive(self, element, base):
         pass
@@ -14,7 +13,10 @@ class Directive(ABC):
         return self.do_directive(element, base)
 
 
-class ExtractDirective(Directive):
+class GetChildDirective(Directive):
+    """
+    Directive to path into an element's children and extract a value
+    """
 
     def __init__(self, *args):
         self.path = args
@@ -24,6 +26,27 @@ class ExtractDirective(Directive):
 
         # Start with the passed-in dictionary element
         node = element
+
+        # Descend into the element's children as specified by self.path
+        for step in self.path:
+            node = node[step]
+
+        return node
+
+class GetFromBaseDirective(Directive):
+    """
+    Similar to the GetChildDirective, but retrieves a value by path 
+    starting from the root of the whole dictionary
+    """
+    
+    def __init__(self, *args):
+        self.path = args
+
+
+    def do_directive(self, element : dict, base : dict):
+
+        # Start with the passed-in dictionary element
+        node = base
 
         # Descend into the element's children as specified by self.path
         for step in self.path:
